1c63cc3c154d0575387bf1e8627355e3b0193866
Sergey_Puchnin <puchnin1996@mail.ru>
2023-07-17 13:52:50 +0300

    Написание кода

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[] {"Hello", "2", "world", ":-)", "1234", "GB", "0_0", "Russia", "69"};
+string[] array2 = new string[array1.Length];
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
